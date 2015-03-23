8e95597b887db915856922c7fd1db09369c61dfb
Nimit Kalra <nimit@nimitkalra.com>
2015-03-23 00:11:53 -0500

    Update examples with optional descriptions and references.

--- examples/everything.js
+++ examples/everything.js
@@ -1,5 +1,25 @@
 var hsc = require('../lib/hsc.js');
 
+// Standard report with bare-bones information.
 hsc.code(200, function(information) {
 	console.log(information);
 });
+
+// Report with descriptions, but no references.
+hsc.code(200, function(information) {
+	console.log(information);
+}, true);
+
+hsc.code(200, function(information) {
+	console.log(information);
+}, true, false);
+
+// Report with references, but no descriptions.
+hsc.code(200, function(information) {
+	console.log(information);
+}, false, true);
+
+// Report with both descriptions and references.
+hsc.code(200, function(information) {
+	console.log(information);
+}, true, true);
