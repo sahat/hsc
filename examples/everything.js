087596e36fef560eae3651f7000c0e57bd402dac
Nimit Kalra <nimit@nimitkalra.com>
2015-03-23 00:39:32 -0500

    Fix code style errors in /examples/everything.js.

--- examples/everything.js
+++ examples/everything.js
@@ -2,30 +2,30 @@ var hsc = require('../lib/hsc.js');
 
 // Standard report with bare-bones information.
 hsc.code(200, function(information) {
-	console.log(information);
+  console.log(information);
 });
 
 // Report with descriptions, but no references.
 hsc.code(200, function(information) {
-	console.log(information);
+  console.log(information);
 }, true);
 
 hsc.code(200, function(information) {
-	console.log(information);
+  console.log(information);
 }, true, false);
 
 // Report with references, but no descriptions.
 hsc.code(200, function(information) {
-	console.log(information);
+  console.log(information);
 }, false, true);
 
 // Report with both descriptions and references.
 hsc.code(200, function(information) {
-	console.log(information);
+  console.log(information);
 }, true, true);
 
 // Errors.
 // Invalid code.
 hsc.code(2500, function(information) {
-	console.log(information);
+  console.log(information);
 });
